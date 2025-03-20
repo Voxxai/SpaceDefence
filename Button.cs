559d6e055f0b5eb5a0341db9d8fde540a510c06f
Voxxai <gilkranendonk@gmail.com>
2025-03-20 15:10:02 +0100

    WORKING! Add Button class and implement start/quit buttons in GameManager

---
+++ Button.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceDefence;
+
+public class Button
+{
+    private Rectangle _rectangle;
+    private readonly string _text;
+    private readonly SpriteFont _font;
+    private readonly Color _textColor = Color.White;
+
+    public event EventHandler Clicked;
+
+    public Button(Rectangle rectangle, string text, SpriteFont font)
+    {
+        _rectangle = rectangle;
+        _text = text;
+        _font = font;
+    }
+
+    public void Update(MouseState mouseState)
+    {
+        if (mouseState.LeftButton == ButtonState.Pressed && _rectangle.Contains(mouseState.X, mouseState.Y))
+            Clicked?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(GameManager.GetGameManager().DummyTexture, _rectangle,
+            Color.Gray); // Use a dummy texture for the button background
+        var textSize = _font.MeasureString(_text);
+        var textPosition = new Vector2(
+            _rectangle.X + (_rectangle.Width - textSize.X) / 2,
+            _rectangle.Y + (_rectangle.Height - textSize.Y) / 2);
+        spriteBatch.DrawString(_font, _text, textPosition, _textColor);
+    }
+}
--- Engine/GameManager.cs
+++ Engine/GameManager.cs
@@ -17,11 +17,18 @@ namespace SpaceDefence
         private List<GameObject> _toBeRemoved;
         private List<GameObject> _toBeAdded;
         private ContentManager _content;
+        private Texture2D _backgroundTexture;
+        private SpriteFont _titleFont;
+        private SpriteFont _buttonFont;
+        private Button _startButton;
+        private Button _quitButton;
 
         public Random RNG { get; private set; }
         public Ship Player { get; private set; }
         public InputManager InputManager { get; private set; }
         public Game Game { get; private set; }
+        public Texture2D DummyTexture { get; private set; }
+        public GameState CurrentGameState { get; private set; }
 
         public static GameManager GetGameManager()
         {
@@ -36,6 +43,7 @@ namespace SpaceDefence
             _toBeAdded = new List<GameObject>();
             InputManager = new InputManager();
             RNG = new Random();
+            CurrentGameState = GameState.StartScreen;
         }
 
         public void Initialize(ContentManager content, Game game, Ship player)
@@ -43,10 +51,44 @@ namespace SpaceDefence
             Game = game;
             _content = content;
             Player = player;
+
+            DummyTexture = new Texture2D(Game.GraphicsDevice, 1, 1);
+            DummyTexture.SetData(new Color[] { Color.White });
+
+            int buttonWidth = 200;
+            int buttonHeight = 50;
+            int centerX = Game.GraphicsDevice.Viewport.Width / 2;
+            int centerY = Game.GraphicsDevice.Viewport.Height / 2;
+            int spacing = 20;
+
+            _startButton = new Button(
+                new Rectangle(centerX - buttonWidth / 2, centerY - buttonHeight - spacing / 2, buttonWidth, buttonHeight),
+                "Start",
+                _buttonFont);
+            _startButton.Clicked += StartButton_Clicked;
+
+            _quitButton = new Button(
+                new Rectangle(centerX - buttonWidth / 2, centerY + spacing / 2, buttonWidth, buttonHeight),
+                "Quit",
+                _buttonFont);
+            _quitButton.Clicked += QuitButton_Clicked;
+        }
+
+        private void StartButton_Clicked(object sender, EventArgs e)
+        {
+            CurrentGameState = GameState.Playing;
+        }
+
+        private void QuitButton_Clicked(object sender, EventArgs e)
+        {
+            CurrentGameState = GameState.Quit; // Or Game.Exit(); depending on your game loop
         }
 
         public void Load(ContentManager content)
         {
+            _backgroundTexture = content.Load<Texture2D>("StartBackground"); // Replace "StartScreenBackground" with the actual asset name
+            _titleFont = content.Load<SpriteFont>("TitleFont"); // Replace "TitleFont" with the actual font asset name
+            _buttonFont = content.Load<SpriteFont>("ButtonFont"); // Replace "ButtonFont" with the actual font asset name
             foreach (GameObject gameObject in _gameObjects)
             {
                 gameObject.Load(content);
@@ -75,12 +117,20 @@ namespace SpaceDefence
                     }
                 }
             }
-            
+
         }
-        
-        public void Update(GameTime gameTime) 
+
+        public void Update(GameTime gameTime)
         {
             InputManager.Update();
+            MouseState mouseState = Mouse.GetState();
+
+            if (CurrentGameState == GameState.StartScreen)
+            {
+                _startButton.Update(mouseState);
+                _quitButton.Update(mouseState);
+                return;
+            }
 
             // Handle input
             HandleInput(InputManager);
@@ -110,20 +160,37 @@ namespace SpaceDefence
             _toBeRemoved.Clear();
         }
 
-        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            foreach (GameObject gameObject in _gameObjects)
+            if (CurrentGameState == GameState.StartScreen)
             {
-                gameObject.Draw(gameTime, spriteBatch);
+                spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, Game.GraphicsDevice.Viewport.Width, Game.GraphicsDevice.Viewport.Height), Color.White);
+
+                string titleText = "Space Defence";
+                Vector2 titleSize = _titleFont.MeasureString(titleText);
+                Vector2 titlePosition = new Vector2(
+                    Game.GraphicsDevice.Viewport.Width / 2 - titleSize.X / 2,
+                    100); // Adjust the Y position as needed
+                spriteBatch.DrawString(_titleFont, titleText, titlePosition, Color.White);
+
+                _startButton.Draw(spriteBatch);
+                _quitButton.Draw(spriteBatch);
+            }
+            else
+            {
+                foreach (GameObject gameObject in _gameObjects)
+                {
+                    gameObject.Draw(gameTime, spriteBatch);
+                }
             }
             spriteBatch.End();
         }
 
         /// <summary>
-        /// Add a new GameObject to the GameManager. 
-        /// The GameObject will be added at the start of the next Update step. 
-        /// Once it is added, the GameManager will ensure all steps of the game loop will be called on the object automatically. 
+        /// Add a new GameObject to the GameManager.
+        /// The GameObject will be added at the start of the next Update step.
+        /// Once it is added, the GameManager will ensure all steps of the game loop will be called on the object automatically.
         /// </summary>
         /// <param name="gameObject"> The GameObject to add. </param>
         public void AddGameObject(GameObject gameObject)
@@ -132,7 +199,7 @@ namespace SpaceDefence
         }
 
         /// <summary>
-        /// Remove GameObject from the GameManager. 
+        /// Remove GameObject from the GameManager.
         /// The GameObject will be removed at the start of the next Update step and its Destroy() mehtod will be called.
         /// After that the object will no longer receive any updates.
         /// </summary>
